6265b144399c3368ec516ae94fc76465f2291b7a
pallaksharma <74041155+pallaksharma@users.noreply.github.com>
2021-05-09 13:11:36 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,21 @@
+class Paper{
+    constructor(x,y,r){
+        var options ={
+            isStatic:false,
+            restitution:0.3,
+            fricition:0,
+            density:1.2
+            }
+        this.body=Bodies.circle(x,y,r,options)
+        this.r=r
+        this.image=loadImage("paper.png");
+        World.add(world,this.body);
+ }
+ display(){
+     push()
+     translate(this.body.position.x,this.body.position.y)
+     imageMode(CENTER)
+     image(this.image,0,0,this.r*2,this.r*2.8);
+     pop()
+ }
+}
